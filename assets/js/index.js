edd845c152aefd1e343bc3ad1e490075720aad00
Niki Tester <bloatedniknak@gmail.com>
2021-12-11 23:29:46 +0000

    hides sections on load

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
-const sections = document.querySelectorAll('section');
+const sections = [...document.querySelectorAll('section')]
+
 sections.forEach(section => {
+    if (section.id === 'messageBox') return
     if (section.id === 'mainMenu') return
     section.style.display = 'none';
 })
