ba3b33c41d49165f9bec197368efdb48c52713ba
Niki Tester <bloatedniknak@gmail.com>
2021-12-12 00:19:20 +0000

    add semicolons

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,66 +1,75 @@
 let MESSAGE_TIME_OUT;
 
-const sections = [...document.querySelectorAll('section')]
-const messageBox = sections.filter(section => section.id === 'messageBox')[0]
-const messageClose = document.getElementById('messageClose')
-const buttons = [...document.querySelectorAll('button')]
+const sections = [...document.querySelectorAll('section')];
+const messageBox = sections.filter(section => section.id === 'messageBox')[0];
+const messageClose = document.getElementById('messageClose');
+const buttons = [...document.querySelectorAll('button')];
 
-buttons.forEach(button => button.addEventListener('click', buttonHandler))
-messageClose.addEventListener('click', closeMessage)
+buttons.forEach(button => button.addEventListener('click', buttonHandler));
+messageClose.addEventListener('click', closeMessage);
 
 sections.forEach(section => {
-    if (section.id === 'messageBox') return
-    if (section.id === 'mainMenu') return
-    section.style.display = 'none'
-})
+    if (section.id === 'messageBox') return;
+    if (section.id === 'mainMenu') return;
+    section.style.display = 'none';
+});
 
 function buttonHandler(e) {
+    if (e.currentTarget.classList.contains('menuButton')) {
+        e.currentTarget.parentElement.style.display = 'none';
+        sections.filter(section => section.id === 'mainMenu')[0]
+            .style.display = 'flex';
+        return;
+    }
+
     switch (e.currentTarget.id) {
         case 'playButton':
-            e.currentTarget.parentElement.style.display = 'none'
-            sections.filter(section => section.id === 'gameWindow')[0].style.display = 'flex'
-            break
+            e.currentTarget.parentElement.style.display = 'none';
+            sections.filter(section => section.id === 'gameWindow')[0]
+                .style.display = 'flex';
+            break;
 
         case 'settingsButton':
-            e.currentTarget.parentElement.style.display = 'none'
-            sections.filter(section => section.id === 'settingsMenu')[0].style.display = 'flex'
-            break
+            e.currentTarget.parentElement.style.display = 'none';
+            sections.filter(section => section.id === 'settingsMenu')[0]
+                .style.display = 'flex';
+            break;
 
         case 'howToPlayButton':
-            e.currentTarget.parentElement.style.display = 'none'
-            sections.filter(section => section.id === 'howToPlayMenu')[0].style.display = 'flex'
-            break
+            e.currentTarget.parentElement.style.display = 'none';
+            sections.filter(section => section.id === 'howToPlayMenu')[0]
+                .style.display = 'flex';
+            break;
+        case 'gameMenuButton':
+            e.currentTarget.parentElement.parentElement.style.display = 'none';
+            sections.filter(section => section.id === 'mainMenu')[0]
+                .style.display = 'flex';
+            break;
 
         default:
-            if (e.currentTarget.classList.contains('menuButton')) {
-                e.currentTarget.parentElement.style.display = 'none'
-                sections.filter(section => section.id === 'mainMenu')[0].style.display = 'flex'
-            } else {
-                messageHandler(`${e.currentTarget.id} button not handled`, 'error')
-            }
+            messageHandler(`${e.currentTarget.id} button not handled`, 'error');
     }
 }
 
-
 function messageHandler(message, type) {
-    messageBox.classList.add(type)
+    messageBox.classList.add(type);
     messageBox.style.transform = 'translateY(0px)';
-    messageBox.firstElementChild.textContent = message
+    messageBox.firstElementChild.textContent = message;
     MESSAGE_TIME_OUT = setTimeout(() => {
-        clearStyles(type)
+        clearStyles(type);
     }, 10000);
 }
 
 function closeMessage() {
     window.clearTimeout(MESSAGE_TIME_OUT);
-    const type = messageBox.classList[0]
-    clearStyles(type)
+    const type = messageBox.classList[0];
+    clearStyles(type);
 }
 
 function clearStyles(type) {
-    messageBox.style.removeProperty('transform')
+    messageBox.style.removeProperty('transform');
     setTimeout(() => {
-        messageBox.classList.remove(type)
-        messageBox.firstElementChild.textContent = ''
+        messageBox.classList.remove(type);
+        messageBox.firstElementChild.textContent = '';
     }, 500);
 }
