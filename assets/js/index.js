a9b769182a7e308933cb1a80d29b64d861ba6c81
Niki Tester <bloatedniknak@gmail.com>
2021-12-16 16:04:33 +0000

    improve getImages / getPlayerImages readability by refactoring

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,7 +22,7 @@ function buttonHandler(e) {
 
     switch (e.currentTarget.id) {
         case 'playButton':
-            checkUserData(e)
+            checkUserData(e);
             break;
 
         case 'startGame':
@@ -51,7 +51,7 @@ function buttonHandler(e) {
 }
 
 function messageHandler(message, type) {
-    window.clearTimeout(MESSAGE_TIME_OUT)
+    window.clearTimeout(MESSAGE_TIME_OUT);
     messageBox.classList.add(type);
     messageBox.style.transform = 'translateY(0px)';
     messageBox.firstElementChild.textContent = message;
@@ -86,7 +86,7 @@ function windowHandler(e, sectionID) {
 
 function clearStorage() {
     const message =
-        'Are you sure you want to clear your local storage, you will lose all game progress.'
+        'Are you sure you want to clear your local storage, you will lose all game progress.';
     if (confirm(message)) {
         localStorage.clear();
     }
@@ -94,23 +94,25 @@ function clearStorage() {
 
 function checkUserData(e) {
     if (localStorage.length === 0) {
-        getPlayerImages()
-        windowHandler(e, 'userData')
-    } else windowHandler(e, 'gameWindow')
+        getPlayerImages();
+        windowHandler(e, 'userData');
+    } else startGame(e);
 }
 
-function images(type, callback) {
+function getImages(type, callback) {
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             const responseText = xhttp.responseText;
             const parser = new DOMParser();
-            const doc = parser.parseFromString(responseText, 'text/html')
+            const doc = parser.parseFromString(responseText, 'text/html');
             const files = doc.getElementById('files');
-            const list = [...files.children].filter(item => item.firstElementChild.textContent != '..')
-            if (callback) callback(list);
+            const imageURL = [...files.children]
+                .filter(item => item.firstElementChild.textContent != '..');
+            if (callback) callback(imageURL);
         }
     };
+
     xhttp.open("GET", `assets/img/${type}`, true);
     xhttp.send();
 }
@@ -118,58 +120,88 @@ function images(type, callback) {
 function getPlayerImages() {
     const charSelection = document.getElementById('characterSelection');
     charSelection.innerHTML = '';
-    images('players', result => {
+
+    getImages('players', result => {
         for (let i = 0; i < result.length; i++) {
-            const element = result[i];
-            const imageElem = document.createElement('img');
-            const src = `assets/img/players/${element.firstElementChild.children[0].textContent}`
-            imageElem.src = src;
-            imageElem.classList.add('character');
-            const charName = element.firstElementChild.children[0].textContent.replace('.webp', '');
-            imageElem.id = charName
-            charSelection.append(imageElem);
+            createImgElement(result[i]);
         }
-
-        const characters = document.querySelectorAll('.character');
-
-        characters.forEach(character => {
-            character.addEventListener('click', e => {
-                characters.forEach(character => {
-                    character.classList.remove('selected')
-                })
-                e.currentTarget.classList.add('selected')
-            });
-        })
+        addCharEventListeners();
     });
+
+}
+
+function characterSelection() {
+    const characters = document.querySelectorAll('.character');
+
+    if (this.classList.contains('selected')) {
+        this.classList.remove('selected');
+        return;
+    }
+
+    characters.forEach(character => {
+        character.classList.remove('selected');
+    });
+
+    this.classList.add('selected');
+}
+
+function addCharEventListeners() {
+    const characters = document.querySelectorAll('.character');
+
+    characters.forEach(character => {
+        character.addEventListener('click', characterSelection);
+    });
+}
+
+function createImgElement(image) {
+    const charSelection = document.getElementById('characterSelection');
+    const imageElem = document.createElement('img');
+    const src = `assets/img/players/${image.firstElementChild.children[0].textContent}`;
+
+    imageElem.src = src;
+    imageElem.classList.add('character');
+    imageElem.id = image.firstElementChild.children[0].textContent.replace('.webp', '');
+    charSelection.append(imageElem);
 }
 
 function validateInput(e) {
     const formInput = document.getElementById('username');
     const characters = [...document.querySelectorAll('.character')];
     const selectedCharacter = characters.filter(char => char.classList.contains('selected'))[0];
+    const username = formInput.value.trim();
+    const myRegex = /^[A-Za-z ]+$/g;
 
-    if (formInput.value.length === 0) {
-        messageHandler('Please enter your name!', 'warn')
-        return
+    if (username.length === 0) {
+        messageHandler('Please enter your name!', 'warn');
+        formInput.value = username;
+        return;
     }
 
-    if (formInput.value.length > 12) {
-        messageHandler('Your name is too long, Maximum of 12 Characters!', 'warn')
-        return
+    if (username.length > 12) {
+        messageHandler('Your name is too long, Maximum of 12 Characters!', 'warn');
+        formInput.value = username;
+        return;
     }
-    if (formInput.value.length < 3) {
-        messageHandler('Your name is too short, Minimum of 3 Characters!', 'warn')
-        return
+    if (username.length < 3) {
+        messageHandler('Your name is too short, Minimum of 3 Characters!', 'warn');
+        formInput.value = username;
+        return;
+    }
+
+    if (!username.match(myRegex)) {
+        messageHandler('Your name must only contain upper or lower case letters!', 'warn');
+        formInput.value = username;
+        return;
     }
 
     if (!selectedCharacter) {
-        messageHandler('Please select your character!', 'warn')
-        return
+        messageHandler('Please select your character!', 'warn');
+        return;
     }
 
-    saveUserData(formInput.value, selectedCharacter.id)
+    saveUserData(username, selectedCharacter.id);
     startGame(e);
-    formInput.value = ''
+    formInput.value = '';
 }
 
 function saveUserData(username, character) {
@@ -178,5 +210,10 @@ function saveUserData(username, character) {
 }
 
 function startGame(e) {
-    windowHandler(e, 'gameWindow')
+    const playerImageElement = document.getElementById('playerImg');
+    playerImageElement.src = 'assets/img/players/ratling.webp';
+    const playerNameElement = document.getElementById('playerName');
+    const playerName = localStorage.getItem('username');
+    playerNameElement.innerHTML = `<span>Name: </span> ${playerName}`;
+    windowHandler(e, 'gameWindow');
 }
