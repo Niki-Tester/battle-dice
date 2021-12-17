135548f28ed9a0f65d80461be1df1c481b80d931
Niki Tester <bloatedniknak@gmail.com>
2021-12-17 14:35:45 +0000

    testing module import

---
+++ assets/data/playerImages.js
@@ -0,0 +1,31 @@
+const playerImages = [{
+        name: "Hag",
+        fileName: "hagmatriarch.webp"
+    },
+    {
+        name: "Planar",
+        fileName: "planaradept.webp"
+    },
+    {
+        name: "Ratling",
+        fileName: "ratling.webp"
+    },
+    {
+        name: "Guardian",
+        fileName: "sanctumgardian.webp"
+    },
+    {
+        name: "Giant",
+        fileName: "skygiant.webp"
+    },
+    {
+        name: "Vampire",
+        fileName: "vampirechilder.webp"
+    },
+    {
+        name: "Werewolf",
+        fileName: "werewolfcub.webp"
+    }
+]
+
+export default playerImages;
