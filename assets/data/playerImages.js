b7dd5938315b7be285f3f903c1a1ef4298da18e5
Niki Tester <bloatedniknak@gmail.com>
2021-12-24 11:05:27 +0000

    add id's to playerImages object

--- assets/data/playerImages.js
+++ assets/data/playerImages.js
@@ -1,28 +1,35 @@
 const playerImages = [{
+        id: "hagmatriarch",
         name: "Hag",
         fileName: "hagmatriarch.webp"
     },
     {
+        id: "planaradept",
         name: "Planar",
         fileName: "planaradept.webp"
     },
     {
+        id: "ratling",
         name: "Ratling",
         fileName: "ratling.webp"
     },
     {
+        id: "sanctumguardian",
         name: "Guardian",
         fileName: "sanctumguardian.webp"
     },
     {
+        id: "skygiant",
         name: "Giant",
         fileName: "skygiant.webp"
     },
     {
+        id: "vampirechilder",
         name: "Vampire",
         fileName: "vampirechilder.webp"
     },
     {
+        id: "werewolfcub",
         name: "Werewolf",
         fileName: "werewolfcub.webp"
     }
