a3ad2ad5dfc17410f1997dfaa2cd72a6f8dcc763
Niki Tester <bloatedniknak@gmail.com>
2021-12-31 12:29:18 +0000

    add IDs to opponent images

--- assets/data/opponentImages.js
+++ assets/data/opponentImages.js
@@ -1,16 +1,20 @@
 const opponentImages = [{
+        id: "ooze",
         name: "Ooze",
         fileName: "ooze.webp"
     },
     {
+        id: "theinevitable",
         name: "The Inevitable",
         fileName: "theinevitable.webp"
     },
     {
+        id: "nightmother",
         name: "Night Mother",
         fileName: "thenightmother.webp"
     },
     {
+        id: "witchking",
         name: "Witch King",
         fileName: "witchking.webp"
     }
