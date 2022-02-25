1b432ee234c30c0617a527ac8e37db187893df57
Niki Tester <bloatedniknak@gmail.com>
2022-02-25 19:13:00 +0000

    change default hp from 20 to 10

--- assets/js/Character.js
+++ assets/js/Character.js
@@ -1,5 +1,5 @@
 class Character {
-    constructor(name, charIMG, hp = 20, level = 0, dmgMultiplier = 1, roll = null) {
+    constructor(name, charIMG, hp = 10, level = 0, dmgMultiplier = 1, roll = null) {
         this.name = name;
         this.charIMG = charIMG;
         this.hp = hp;
