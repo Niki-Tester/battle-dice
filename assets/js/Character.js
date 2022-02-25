f1dc3493fba8d738cdb84ab66724558647fb4793
Niki Tester <bloatedniknak@gmail.com>
2022-02-25 20:16:24 +0000

    add whitespace

--- assets/js/Character.js
+++ assets/js/Character.js
@@ -1,12 +1,12 @@
 class Character {
-    constructor(name, charIMG, hp = 10, level = 0, dmgMultiplier = 1, roll = null) {
-        this.name = name;
-        this.charIMG = charIMG;
-        this.hp = hp;
-        this.level = level;
-        this.dmgMultiplier = dmgMultiplier;
-        this.roll = roll;
-    }
+	constructor(name, charIMG, hp = 10, level = 0, dmgMultiplier = 1, roll = null) {
+		this.name = name;
+		this.charIMG = charIMG;
+		this.hp = hp;
+		this.level = level;
+		this.dmgMultiplier = dmgMultiplier;
+		this.roll = roll;
+	}
 }
 
 export default Character;
