f60900d684b93d91c97c550b7183ae23809876fe
Niki Tester <bloatedniknak@gmail.com>
2022-02-26 01:49:21 +0000

    add addition music / sfx

---
+++ assets/data/audioFiles.js
@@ -0,0 +1,18 @@
+const audioFiles = [{
+    path: "assets/audio/menuTheme.mp3"
+},
+{
+    path: "assets/audio/battleThemeA.mp3"
+},
+{
+    path: "assets/audio/battleThemeB.mp3"
+},
+{
+    path: "assets/audio/battleThemeC.mp3"
+},
+{
+    path: "assets/audio/battleThemeD.mp3"
+}
+]
+
+export default audioFiles;
